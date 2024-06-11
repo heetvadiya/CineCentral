a1a350c2d29c86b564260378f0bb10be8f86eaac
heetvadiya <heetvadiya@gmail.com>
2024-06-11 11:51:14 +0530

    added MembershipType and Realtion to customer

--- CineCentral/Models/Customer.cs
+++ CineCentral/Models/Customer.cs
@@ -9,5 +9,14 @@ namespace CineCentral.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; } // Navigation property
+        // it helps navigate from one to another
+        // useful , when we want to load an object and its related object from Dbase
+        public byte MembershipTypeId { get; set; } // Foreign Key
+        // for optimisation, we sometimes dont want to include entire object,
+        // We just want to include foreignkey , we can do that by this property
+        // EF automatically recognises it by the convention
+
     }
 }
