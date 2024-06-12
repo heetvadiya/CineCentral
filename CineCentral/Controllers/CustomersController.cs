d20dc24ccc1b08fdbf67f065fec4279246535664
heetvadiya <heetvadiya@gmail.com>
2024-06-12 17:28:41 +0530

    Displayed details

--- CineCentral/Controllers/CustomersController.cs
+++ CineCentral/Controllers/CustomersController.cs
@@ -31,7 +31,7 @@ namespace CineCentral.Controllers
 
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
