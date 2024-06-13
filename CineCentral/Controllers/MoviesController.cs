0d7b902a5ae438a334313323b2b2a5891a299fab
heetvadiya <heetvadiya@gmail.com>
2024-06-13 23:58:24 +0530

    Update+Add new Movie

--- CineCentral/Controllers/MoviesController.cs
+++ CineCentral/Controllers/MoviesController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using CineCentral.Models;
 using System.Data.Entity;
+using CineCentral.ViewModels;
 //using CineCentral.ViewModels;
 
 namespace CineCentral.Controllers
@@ -45,6 +46,56 @@ namespace CineCentral.Controllers
             return View(movie);
         }
 
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
 
     }
 }
--- CineCentral/Models/Movie.cs
+++ CineCentral/Models/Movie.cs
@@ -13,12 +13,17 @@ namespace CineCentral.Models
         [StringLength(255)]
         public string Name { get; set; }
         public Genre Genre { get; set; } // Navigation Property
+
+        [Display(Name = "Genre")]
+        [Required]
         public byte GenreId { get; set; } //Foreign Key
 
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
 
     }
